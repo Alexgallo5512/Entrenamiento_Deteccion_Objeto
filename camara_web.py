e87321cd997b1dbf2b391072ccbec015e45b9a04
Agallo <agallo5512@gmail.com>
2025-12-18 22:26:01 +0000

    Update 18 12 2025

--- camara_web.py
+++ camara_web.py
@@ -1,16 +1,17 @@
 import os 
 import cv2
-import logging
 from ultralytics import YOLO
-os.system('cls')
-logger = logging.getLogger(__name__)
-AM_I_IN_A_DOCKER_CONTAINER: bool = os.environ.get("AM_I_IN_A_DOCKER_CONTAINER", False)
+os.system('clear')
+
+
+
+SAVE_PATH = "/home/icam-540/captura/captura_sin_tapa_2.jpg"   # ruta donde guardar
 
 
 def main():
-    logger.info(AM_I_IN_A_DOCKER_CONTAINER)
+    
     model = YOLO("best.pt")
-    cap=cv2.VideoCapture(1)
+    cap=cv2.VideoCapture(0)
     while cap.isOpened():
         # Leemos el frame del video
         ret, frame = cap.read()
@@ -22,12 +23,25 @@ def main():
         annotated_frame = results[0].plot()
         #print(annotated_frame)
         # Visualizamos los resultados
+
+        detected_classes = results[0].boxes.cls.tolist()  # IDs detectados
+
+        # Buscar si el nombre "Sin_Tapa" aparece
+        for cls_id in detected_classes:
+            class_name = results[0].names[int(cls_id)]
+            if class_name == "Sin_Tapa":
+                cv2.imwrite(SAVE_PATH, annotated_frame)
+               
+                break
+
         cv2.imshow("YOLO Inference", annotated_frame)
         # El ciclo se rompe al presionar "Esc"
         if cv2.waitKey(1) & 0xFF == 27:
             break
     cap.release() 
     cv2.destroyAllWindows()
+
+
 
 if __name__ == "__main__":
     main()
---
+++ camara_web_servicio_web.py
@@ -0,0 +1,51 @@
+import os
+import cv2
+from flask import Flask, Response
+from ultralytics import YOLO
+
+os.system('clear')
+
+app = Flask(__name__)
+model = YOLO("best.pt")
+cap = cv2.VideoCapture(0)
+
+SAVE_PATH = "/home/icam-540/captura/captura_sin_tapa.jpg"   # ruta donde guardar
+
+
+def generate():
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model(frame)
+        annotated = results[0].plot()
+
+        # -----------------------------------------------------
+        # Detectar si aparece la clase "Sin_Tapa" en el frame
+        # -----------------------------------------------------
+        detected_classes = results[0].boxes.cls.tolist()  # IDs detectados
+
+        # Buscar si el nombre "Sin_Tapa" aparece
+        for cls_id in detected_classes:
+            class_name = results[0].names[int(cls_id)]
+            if class_name == "Sin_Tapa":
+                cv2.imwrite(SAVE_PATH, annotated)
+               
+                break
+
+        ret, buffer = cv2.imencode('.jpg', annotated)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video')
+def video():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090)
