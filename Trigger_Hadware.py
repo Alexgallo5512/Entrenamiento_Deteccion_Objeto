f81ef8fa5b832086021c8b6cadb228b0e1f54d9c
Alexgallo5512 <alexgallo5512@gmail.com>
2026-03-09 15:34:29 -0500

    Nueva Prueba de Hadware Trigger

--- Trigger_Hadware.py
+++ Trigger_Hadware.py
@@ -17,8 +17,8 @@ from ultralytics import YOLO
 MODEL_PATH = "best.pt"
 
 # ✅ Rutas Compatible Windows/Linux
-SAVE_PATH = "/home/icam-540/capturas/captura_sin_tapa.jpg"
-
+SAVE_PATH = "/home/icam-540/capturas"
+FILE_NAME = "captura_sin_tapa.jpg"
 # Resolución cámara (reducida para mejor rendimiento)
 WIDTH  = 1280   
 HEIGHT = 720   
@@ -76,16 +76,19 @@ def new_image_handler(sample):
             latest_frame = frame
 
         cam_count += 1
-        
+        filepath = os.path.join(SAVE_PATH, FILE_NAME)
+        cv2.imwrite(filepath, latest_frame)
+
+        print("📸 Imagen guardada:", filepath)
     except Exception as e:
         print(f"❌ Error en callback cámara: {e}")
 
 
-def save_detection(frame, class_name="Sin_Tapa"):
+def save_detection(filepath,frame, class_name="Sin_Tapa"):
     """Guarda imagen de detección"""
     try:
-        cv2.imwrite(str(SAVE_PATH), frame)
-        print(f"✅ Detección guardada: {SAVE_PATH}")
+        cv2.imwrite(filepath, frame)
+        print(f"✅ Detección guardada: {filepath}")
         detection_event.set()
     except Exception as e:
         print(f"❌ Error al guardar: {e}")
@@ -121,7 +124,7 @@ if __name__ == "__main__":
 
         # ========== CONFIGURAR PIPELINE ==========
         pipe_params = {
-            "acq_mode": 0,
+            "acq_mode": 2,
             "width": WIDTH,
             "height": HEIGHT,
             "enable_infer": 0  # IMPORTANTE: sin inferencia en cámara
@@ -132,20 +135,25 @@ if __name__ == "__main__":
 
         cn2.advcam_config_pipeline(camera, **pipe_params)
         cn2.advcam_open(camera, -1)
+        # Setting do0 parameters
+        camera.dio.do0.op_mode = 0 # DO op mode: user output
+        camera.dio.do0.user_output = 0 # DO low, DI high
+        camera.dio.do0.reverse = 0
+
         cn2.advcam_register_new_image_handler(camera, new_image_handler)
 
         # ========== CONFIGURACIÓN ÓPTICA ==========
         camera.lighting.selector = 2
         camera.lighting.gain = 100
-        cn2.advcam_set_img_sharpness(camera, 90)
-        cn2.advcam_set_img_brightness(camera, 50)
-        cn2.advcam_set_img_gain(camera, 5)
+        #  cn2.advcam_set_img_sharpness(camera, 90)
+        # cn2.advcam_set_img_brightness(camera, 50)
+        # cn2.advcam_set_img_gain(camera, 5)
         camera.set_acq_frame_rate(TARGET_FPS)
 
         # ========== INICIAR CAPTURA ==========
-        
         camera.focus.pos_zero()
         time.sleep(0.5)
+        
         i = 0
         while i < 30:
             camera.focus.direction = 1 # lens focusing motor backward
@@ -156,72 +164,84 @@ if __name__ == "__main__":
                 print("valor ", i)
             except ValueError:
                 print("lens position out of index")
+
         cn2.advcam_play(camera)
+    # ---------- START STREAM ----------
+
         print("▶️  Sistema iniciado. Presiona ESC para salir")
         print(f"   📊 Resolución cámara: {WIDTH}x{HEIGHT}")
         print(f"   🧠 Tamaño YOLO: {YOLO_SIZE}x{YOLO_SIZE}")
         print(f"   ⚙️  Confianza YOLO: {YOLO_CONF}")
-        print()
 
-        # ================= LOOP PRINCIPAL =================
+       # ================= LOOP PRINCIPAL =================
         while True:
             # Obtener frame más reciente (con lock)
             with frame_lock:
                 if latest_frame is None:
                     time.sleep(0.01)  # Esperar frame
                     continue
-                frame = latest_frame.copy()
+                else:
+                     frame = '/home/icam-540/capturas/captura_sin_tapa.jpg'
 
             # -------- REDIMENSIONAR PARA YOLO --------
             # IMPORTANTE: esto acelera mucho la inferencia
-            resized = cv2.resize(frame, (YOLO_SIZE, YOLO_SIZE))
+                #resized = cv2.resize(frame, (YOLO_SIZE, YOLO_SIZE))
             
             # -------- INFERENCIA YOLO --------
-            results = model(
-                resized, 
-                verbose=False,
-                conf=YOLO_CONF  # Confianza mínima = más rápido
-            )
-
-            yolo_count += 1
+                results = model(
+                    frame, 
+                    verbose=True,
+                    conf=YOLO_CONF  # Confianza mínima = más rápido
+                )
+                results[0].show()
+                yolo_count += 1
 
             # -------- DIBUJAR RESULTADOS --------
-            annotated = results[0].plot()
+                annotated = results[0].plot()
 
             # -------- LÓGICA DE DETECCIÓN --------
-            detected = False
-            for cls_id in results[0].boxes.cls.tolist():
-                class_name = results[0].names[int(cls_id)]
-                if class_name == "Sin_Tapa":
-                    print(f"🎯 Objeto detectado: {class_name}")
-                    save_detection(annotated, class_name)
-                    detected = True
-                    break
+                detected = False
+                for cls_id in results[0].boxes.cls.tolist():
+                    class_name = results[0].names[int(cls_id)]
+                    if class_name == "Sin_Tapa":
+                        salida  = str(camera.dio.do0.user_output)
+                        print(salida)
+                        time.sleep(1)
+                        camera.dio.do0.user_output = 0
+                        print(f"🎯 Objeto detectado: {class_name}")
+                        cv2.imshow("iCAM-540 + YOLO (Optimizado)", annotated)
+                        save_detection(SAVE_PATH,annotated, class_name)
+                        detected = True
+                        camera.dio.do0.user_output = 1
+                        salida  = str(camera.dio.do0.user_output)
+                        print(salida)
+                        time.sleep(1)
+                        break
 
             # -------- MOSTRAR EN PANTALLA --------
             # Agregar info de FPS
             #info_text = f"CAM: {cam_fps} FPS | YOLO: {yolo_fps} FPS"
-            cv2.putText(
-                annotated, 
-                '', 
-                (10, 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.7,
-                (0, 255, 0),
-                2
-            )
+                cv2.putText(
+                    annotated, 
+                    '', 
+                    (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7,
+                    (0, 255, 0),
+                    2
+                )
             
-            cv2.imshow("iCAM-540 + YOLO (Optimizado)", annotated)
+            
 
             # -------- CONTAR FPS --------
             now = time.time()
             if now - last_fps_time >= 1.0:
-                cam_fps = cam_count
-                yolo_fps = yolo_count
-                cam_count = 0
-                yolo_count = 0
-                last_fps_time = now
-                print(f"📊 FPS → Cámara: {cam_fps} | YOLO: {yolo_fps}")
+                    cam_fps = cam_count
+                    yolo_fps = yolo_count
+                    cam_count = 0
+                    yolo_count = 0
+                    last_fps_time = now
+                    print(f"📊 FPS → Cámara: {cam_fps} | YOLO: {yolo_fps}")
 
             # -------- CONTROLES TECLADO --------
             key = cv2.waitKey(1) & 0xFF
